c9e4bec32ca9367f902119b6af69be5fb7adde81
rachelnova18 <rachelnv26@gmail.com>
2025-05-13 12:07:32 +0700

    deklarasi pointer next dan prev untuk petunjuk ke data sebelum dan sesudah

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -7,4 +7,8 @@ struct Node
     // deklarasi noMhs dan name untuk menampung data
     int noMhs;
     string name;
-    
+    // deklarasi pointer next dan prev untuk petunjuk ke data sebelum dan sesudahnya
+    Node *next;
+    Node *prev;
+};
+
