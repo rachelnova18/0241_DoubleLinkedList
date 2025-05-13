c77be01ed8b1ef45a15884ed0c5148d91eec4227
rachelnova18 <rachelnv26@gmail.com>
2025-05-13 14:18:17 +0700

    prosedur untuk menampilkan data secara urutan terbalik

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -179,3 +179,23 @@ void traverse()
     }
 }
 
+// prosedur untuk menampilkan data secara urutan terbalik
+void revtraverse()
+{
+    if (listEmpty())
+        cout << "\nList is empty" << endl;
+    else
+    {
+        cout << "\nRecords in descending order of roll number are: " << endl;
+        Node *currentNode = START;
+        while (currentNode->next != NULL)
+            currentNode = currentNode->next;
+
+        while (currentNode != NULL)
+        {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;
+            currentNode = currentNode->prev;
+        }
+    }
+}
+
